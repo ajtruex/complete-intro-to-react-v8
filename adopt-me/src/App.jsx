2e82abfdcbf4cbb8772ecc09b8ca165e649cfb55
Andrew Truex <andrewtruex@gmail.com>
2022-12-20 10:20:24 -0500

    04-hooks finished

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client"
 import Pet from "./Pet"
+import SearchParams from "./SearchParams"
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
+      <SearchParams />
       <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
       <Pet name="Doink" animal="cat" breed="Mixed" />
